682379848930106b04b5a5d635082fb5165a50c8
Anil Madhavapeddy <anil@recoil.org>
2016-05-31 16:28:30 +0100

    run: tweak the container run to be more succinct

--- run.sh
+++ run.sh
@@ -1,20 +1,21 @@
 #!/bin/sh -e
+# Run TWiOPAM in a container
 
 WRKDIR=/home/opam
 TMPREPO=${WRKDIR}/opam-repository
-OUTDIR=${WRKDIR}/out
+OUTDIR=/out
 
 if [ ! -d ${TMPREPO} ]; then
-  git clone git://github.com/ocaml/opam-repository ${TMPREPO}
+  git clone -q git://github.com/ocaml/opam-repository ${TMPREPO}
 else
-  git -C ${TMPREPO} pull origin master
+  git -q -C ${TMPREPO} pull origin master
 fi
 
 mkdir -p out/cache
 export OPAMROOT=/tmp/.local-opam
 if [ -d ${OPAMROOT} ]; then
-  opam update -u -y
+  opam update -q -u -y
 else
-  opam init -y -k git ${TMPREPO}
+  opam init -q -y -k git ${TMPREPO}
 fi
 twiopam -d ${OUTDIR} $* path:${TMPREPO}
