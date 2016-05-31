0bb25e92302e983a77d3408200c79d1e7f366fdc
Anil Madhavapeddy <anil@recoil.org>
2016-05-31 14:40:10 +0100

    install twiopam-run

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+WRKDIR=/home/opam
+TMPREPO=${WRKDIR}/opam-repository
+OUTDIR=${WRKDIR}/out
+
+if [ ! -d ${TMPREPO} ]; then
+  git clone git://github.com/ocaml/opam-repository ${TMPREPO}
+else
+  git -C ${TMPREPO} pull origin master
+fi
+
+mkdir -p out/cache
+export OPAMROOT=/tmp/.local-opam
+if [ -d ${OPAMROOT} ]; then
+  opam update -u -y
+else
+  opam init -y -k git ${TMPREPO}
+fi
+./twiopam.native -d ${OUTDIR} $* path:${TMPREPO} 
