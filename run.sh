9c59a14be5504282b4254ac44e33c10979ddae9c
Anil Madhavapeddy <anil@recoil.org>
2016-06-16 23:59:15 +0100

    remove git -q

--- run.sh
+++ run.sh
@@ -5,10 +5,19 @@ WRKDIR=/home/opam
 TMPREPO=${WRKDIR}/opam-repository
 OUTDIR=/out
 
+case $1 in
+--help)
+  twiopam --help
+  exit 0
+  ;;
+*)
+  ;;
+esac
+
 if [ ! -d ${TMPREPO} ]; then
-  git clone -q git://github.com/ocaml/opam-repository ${TMPREPO}
+  git clone git://github.com/ocaml/opam-repository ${TMPREPO}
 else
-  git -q -C ${TMPREPO} pull origin master
+  git -C ${TMPREPO} pull origin master
 fi
 
 mkdir -p out/cache
